1974cdb605bfd68481ab2cdd8713be7e9b4397b0
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-10 13:40:25 +0200

    Add files via upload

---
+++ jednakostranicni_trougao_datog_obima/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace jednakostranicni_trougao_datog_obima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, o, a1;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+
+            o = a + b + c;
+            a1 = o / 3;
+
+            if ((o % 3) > 0)
+            {
+                Console.WriteLine("ne");
+            }
+            else
+            {
+                Console.WriteLine("da {0}",a1);
+            }
+        }
+    }
+}
